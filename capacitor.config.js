af53a52295e29dd7cf8dba10cefa50f274e1abb0
OneTzy <54070834+RidwanXyZ@users.noreply.github.com>
2026-08-19 02:14:45 +0700

    Update and rename capacitor.config.js to capacitor.config.json

--- capacitor.config.js
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.ridwanxyz.pressureconverter",
-  appName: "Pressure Converter",
-  webDir: "www",
-};
-
-export default config;

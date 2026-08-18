6a438c479ca59719483748eeee152440228380f3
OneTzy <54070834+RidwanXyZ@users.noreply.github.com>
2026-08-19 02:08:37 +0700

    Add initial Capacitor configuration for app

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.ridwanxyz.pressureconverter",
+  appName: "Pressure Converter",
+  webDir: "www",
+};
+
+export default config;
